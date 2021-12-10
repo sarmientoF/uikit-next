12a83781e7948663f923ad229c8783c6f2c4f01a
SergeiMeza <sergeimeza@gmail.com>
2021-12-10 12:18:57 +0900

    edit navigation component

--- components/Elements/Navigation/VerticalNavigation/VerticalNavigation.tsx
+++ components/Elements/Navigation/VerticalNavigation/VerticalNavigation.tsx
@@ -5,6 +5,8 @@ import { classNames } from '../../../helpers/methods'
 import { Anchor } from '../../..'
 import { Disclosure } from '@headlessui/react'
 
+import { useRouter } from 'next/router'
+
 export type VerticalNavigationProps = {
   items?: NavigationItem[]
   secondaryNavigationTitle?: string
@@ -20,6 +22,8 @@ const VerticalNavigationComponent: React.FC<VerticalNavigationProps> = ({
   showIcons = true,
   onItemSelected = (item) => console.log('item selected:', item.name),
 }) => {
+  const router = useRouter()
+
   const [navItems, setNavItems] = useState<any[]>(items.slice())
 
   function handleItemChange(value: string) {
@@ -103,7 +107,12 @@ const VerticalNavigationComponent: React.FC<VerticalNavigationProps> = ({
                         key={subItem.name}
                         to={subItem.to}
                         href={subItem.href}
-                        className="group w-full flex items-center pl-10 pr-2 py-2 text-sm font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50"
+                        className={classNames(
+                          subItem.href === router.pathname.split('?')[0]
+                            ? 'bg-primary-100 text-primary-900'
+                            : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
+                          'group w-full  flex items-center pl-10 pr-2 py-2 text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500 focus:border-primary-500',
+                        )}
                       >
                         {subItem.name}
                       </Anchor>
